80e824d901da24e5e0b6d5354e22e4a29ab26871
anatimoteo <analu2807.info@gmail.com>
2025-07-01 16:46:40 -0300

    feat: inclusão do str na model Compra

--- core/models/compra.py
+++ core/models/compra.py
@@ -14,7 +14,14 @@ class Compra(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
     status = models.IntegerField(choices=StatusCompra.choices,  default=StatusCompra.CARRINHO)
 
+
+    def __str__(self):
+        return f"Compra {self.id} - {self.usuario} - {self.get_status_display()}"
+
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
     quantidade = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantidade} x {self.livro.titulo} - Compra {self.compra.id}"
