58c199e4b0a4d7f08b9fe1cf6ff1e8a222b90018
anatimoteo <analu2807.info@gmail.com>
2025-07-01 16:19:07 -0300

    feat: criação dos itens da compra

--- core/models/compra.py
+++ core/models/compra.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from .user import User
+from .livro import Livro
 
 class Compra(models.Model):
     class StatusCompra(models.IntegerChoices):
@@ -8,6 +9,12 @@ class Compra(models.Model):
         FINALIZADO = 2, "Realizado"
         PAGO = 3, "Pago"
         ENTREGUE = 4, "Entregue"
-
+        CANCELADO = 5, "Cancelado"
+        
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="compras")
     status = models.IntegerField(choices=StatusCompra.choices,  default=StatusCompra.CARRINHO)
+
+class ItensCompra(models.Model):
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
+    livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
+    quantidade = models.IntegerField(default=1)
